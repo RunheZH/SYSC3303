a887450678d0074cb500bec0f1774f3e65e53fef
RunheZH <runhe.zhong@gmail.com>
2018-07-15 00:55:22 -0400

    update-es-normal-thread

--- SYSC3303/src/ErrorSimulator/ErrorSimulator.java
+++ SYSC3303/src/ErrorSimulator/ErrorSimulator.java
@@ -12,21 +12,57 @@ import java.util.*;
 
 public class ErrorSimulator{
 	
+	private int userOption;
+	private int operationChoice;
+	private ErrorType error;
+	private ErrorSController controller;
+	
 	public ErrorSimulator() {
+		error = new ErrorType();
+		controller = new ErrorSController();
 		
 	}
-
- 
-  public static void main(String args[]){
-
-    //ErrorSimulator i = new ErrorSimulator();
-
-    while(true) {
-
-    	  //i.sendAndReceive();
-
-    } 
-
-  }
+	
+	
+	public void ErrorMenu() throws IOException{
+		
+		Scanner sc = new Scanner(System.in);
+		error.printErrorMenu();
+	
+		boolean valid = false;
+		
+		while(!valid) {
+			
+			userOption = sc.nextInt();
+			
+			if(userOption >= 0 && userOption <= 9) {
+				this.controller.distribute(userOption);
+				valid = true;
+			}
+			else valid = false;
+			
+		}
+		
+	}
+	
+	
+	public void IllegalOperation() throws IOException{
+		
+		Scanner sc = new Scanner(System.in);
+		error.printIllegalOperation();
+		operationChoice = sc.nextInt(); 
+		//...
+		
+	}
+	
+	public static void main(String args[]) throws IOException{
+		
+		
+		ErrorSimulator i = new ErrorSimulator();
+		
+		while(true) {		
+			i.ErrorMenu();
+		} 
+	}
 
 }
