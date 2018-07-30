f6bf9380a864022497d8cefecd9933b622c9b3ce
georgiana0818 <georgiana0818@gmail.com>
2018-07-29 20:13:12 -0400

    updata-errorSimulatorPackage

--- SYSC3303/src/errorSimulator/UI.java
+++ SYSC3303/src/errorSimulator/UI.java
@@ -6,204 +6,136 @@ package errorSimulator;
 
 import java.util.Scanner;
 
+import ES.ESListener;
+
 public class UI {
 
-	RequestParser RP;
-	String cmd;
-	Scanner in;
-
-	public UI(RequestParser RP) {
-		this.RP = RP;
-		in = new Scanner(System.in);
+	private Scanner scan;
+	private String ec,tc; 
+	//private byte packetChoice;
+	private int errorChoice;
+	private int transError;
+	//private boolean status = false;
+	private ESListener listener;
+	
+	public  UI() {
+		scan = new Scanner(System.in);
 	}
 
-
-
-	/*
-	 * Menu for the Error simulator
-	*/
-	public int mainMenu() {
-
-		int index = 3, num = 0;
-		System.out.println("-------------------New Packet Received------------------");
-		int type = RP.getType();
-		System.out.println("\tPacket Type: " + RP.getTypeName());
-
-		if(type == 1 || type == 2) {
-			System.out.println("\tRequested File: " + RP.getFilename());
-		}else if(type == 3 || type == 4){
-			System.out.println("\tBlock#: " + RP.getBlockNum());
-			if(type == 3) {
-				System.out.println("\tData Length: " + RP.getFileData().length);
-			}
-		}else if(type == 5){
-			System.out.println("\tErrorCode: " + RP.getErrCode());
-			System.out.println("\tError Message: " + RP.getErrMsg());
-		}
-
-		System.out.println("\n\tAvailable Operations:");
-		System.out.println("\t0. Normal Operation");
-		System.out.println("\t1. Modify Op#");	
-
-		if(type == 1 || type == 2) {
-			System.out.println("\t2. Modify Filename");
-			index = 2;
-		}else if(type == 3 || type == 4){
-			System.out.println("\t2. Modify Block#");
-			index = 2;
-			if(type == 3) {
-				System.out.println("\t3. Modify Data");
-				index = 3;
-			}
-		}else if(type == 5){
-			System.out.println("\t2. Modify ErrorCode");
-			System.out.println("\t3. Modify Error Message");
-			index = 3;
-		}
-		cmd = in.next();
-		while(true) {
-			try{
-				num = Integer.parseInt(cmd);
-			} catch (NumberFormatException e) {
-				System.out.println("\t\nInvalid input, please try again.");
-				cmd = in.next();
-				continue;
-			}
-			if(num < 0 || num > index) {
-				System.out.println("\t\nInvalid input, please try again.");
-				cmd = in.next();
-				continue;
-			}else {
-				break;
-			}
-
-		}
-		return num;
-	}
-
-	/*
-	 * Helper method for UI
-	*/
-	public int type2 () {
-		int second = -1, type = RP.getType();
-		if (type == 1 || type ==2) {
-			second = 1;
-		}else if (type == 3 || type ==4) {
-			second = 2;
-		}else if (type == 5) {
-			second = 3;
-		}else {
-			System.out.println("Error.");
-		}
-
-		return second; 
-	}
-
-	/*
-	 * Helper method for UI
-	*/
-	public int type3() {
-		int third = -1, type = RP.getType();
-		if (type == 3) {
-			third = 1;
-		}else if (type == 5) {
-			third = 2;
-		}else {
-			System.out.println("Error");
+	 
+	
+	public void errorMainMenu() {
+		
+		System.out.println("----------Error Selection----------");
+		System.out.println("    0. Normal Operation");
+		System.out.println("    1. Transmission Error");
+		System.out.println("    2. Error Codes (1-6)");
+		System.out.println(">>>>>>>> input quit to exit this program");
+		
+		ec = scan.next();
+		
+		if(ec.equals("0")) {
+			errorChoice = 0;
+		}else if (ec.equals("1")) {
+			errorChoice = 1;
+			transmissionError();
+		}else if (ec.equals("2")) {
+			errorChoice = 2;
+			System.out.println("Error Code");
+		}else if (ec.equals("quit")) {
+			System.exit(0);
 		}
 		
-		return third;
 	}
-
-	/*
-	 * Ask user for new Opcode
-	*/
-	public int askOpCode() {
-		int num = -1;
-		System.out.println("Please enter new Opcode: ");
-		cmd = in.next();
-		while(true) {
-			try{
-				num = Integer.parseInt(cmd);
-				return num;
-			} catch (NumberFormatException e) {
-				System.out.println("\t\nInvalid input, please try again.");
-				cmd = in.next();
-				continue;
-			}
-
+	
+	public void transmissionError(){
+		System.out.println("---------- Transmission Error ----------");
+		System.out.println("    1. Lose a packet");
+		System.out.println("    2. Delay a packet");
+		System.out.println("    3. Duplicate a packet");
+		System.out.println("    4. Back to Error main menu");
+		System.out.println(">>>>>>>> input quit to exit this program");
+		
+		tc = scan.next();
+		
+		switch (tc) {
+			case "1": 
+				transError = 1; 
+				break;
+			case "2": 
+				transError = 2;
+				break;
+			case "3": 
+				transError = 3;
+				break;
+			case "4": 
+				transError = 4;
+				errorMainMenu(); 
+				break;
+			case "quit": 
+				System.exit(0);
+				break;
+			default: 
+				System.out.println("Oops, something is wrong"); 
+				break;
 		}
 	}
 	
-	/*
-	 * Ask user for new file name
-	*/
-	public byte [] askFileName() {
-		String newName = "";
-		System.out.println("Please enter new Filename");
-		newName = in.next();
-		byte [] fileName = newName.getBytes();
-		return fileName;
-	}
-
-	/*
-	 * Ask user for new block number
-	*/
-	public int askBkNumber() {
-		int Bk = -1;
-		System.out.println("Please enter new Block number");
-		cmd = in.next();
-		while(true) {
-			try {
-				Bk = Integer.parseInt(cmd);
-				if (Bk >= 0 && Bk <= 65535) {
-					return Bk;
-				}
-			}catch (NumberFormatException e) {
-				System.out.println("\t\nInvalid input, please try again.");
-				cmd = in.next();
-				continue;
-			}
+	/*public void packetSelection() {
+		System.out.println("---------- Transmission Error ----------");
+		System.out.println("    1. RRQ");
+		System.out.println("    2. WRQ");
+		System.out.println("    3. DATA");
+		System.out.println("    4. ACK");
+		System.out.println("    5. ERROR" );
+		System.out.println("    6. Back to Transmission Menu");
+		System.out.println(">>>>>>>> input quit to exit this program");
+		
+		pc = scan.next();
+		
+		switch (pc) {
+			case "1": 
+				packetChoice = 1;
+				listener.handleNetworkError(transError, packetChoice);
+				break;
+			case "2": 
+				packetChoice = 2;
+				listener.handleNetworkError(transError, packetChoice);
+				break;
+			case "3": 
+				packetChoice = 3;
+				listener.handleNetworkError(transError, packetChoice);
+				break;
+			case "4": 
+				packetChoice = 4;
+				listener.handleNetworkError(transError, packetChoice);
+				break;
+			case "5": 
+				packetChoice = 5;
+				listener.handleNetworkError(transError, packetChoice);
+				break;
+			case "6": 
+				packetChoice = 6;
+				listener.handleNetworkError(transError, packetChoice);
+				break;
+			case "quit": 
+				listener.quit(); 
+				break;
+			default: 
+				System.out.println("Oops, something is wrong"); 
+				break;
 		}
-	}
-
-	/*
-	 * Ask user for new error code
-	*/
-	public int askErrCode() {
-		int code = -1;
-		System.out.println("Please enter new Error code");
-		cmd = in.next();
-		while(true) {
-			try {
-				code = Integer.parseInt(cmd);
-				if (code >= 0 && code <= 7) {
-					return code;
-				}
-			}catch (NumberFormatException e) {
-				System.out.println("\t\nInvalid input, please try again.");
-				cmd = in.next();
-				continue;
-			}
-		}
+	}*/
+	
+	/*public byte getPacketChoice() {
+		return packetChoice;
+	}*/
+	
+	public int getErrorType() {
+		return errorChoice;
 	}
 	
-	
-	public byte [] askData() {
-		String newData = "";
-		System.out.println("Please enter new Data");
-		newData = in.next();
-		byte [] data = newData.getBytes();
-		return data;
-	}
-
-	
-
-	public byte [] askErrMSG() {
-		String message = "";
-		System.out.println("Please enter new Error message");
-		message = in.next();
-		byte [] MSG = message.getBytes();
-		return MSG;
+	public int getTransError() {
+		return transError;
 	}
 }
